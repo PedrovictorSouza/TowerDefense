c9043af2e6bb6401f8460c02a7805e552cb391ca
PEDRO VICTOR <48770714+PedrovictorSouza@users.noreply.github.com>
2024-04-28 18:56:12 -0300

    PlayerState started

---
+++ IdleState.gd
@@ -0,0 +1,17 @@
+extends Node2D
+class_name PlayerStates
+
+
+func enter(_player):
+	print("ai papai")
+
+func exit(player):
+	pass
+
+func handle_input(player, event):
+	pass
+
+func update(player, delta):
+	pass
+
+
